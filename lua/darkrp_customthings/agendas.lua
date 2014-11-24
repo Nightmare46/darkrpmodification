27b2a9e91f4cc556260833bb7f2281c50f551323
Gabe Eisenmenger <Gabe.Eisenmenger@gmail.com>
2014-11-24 15:53:52 -0600

    Update agendas.lua

--- lua/darkrp_customthings/agendas.lua
+++ lua/darkrp_customthings/agendas.lua
@@ -7,5 +7,7 @@ Agenda's can be set by the agenda manager and read by both the agenda manager an
 HOW TO MAKE AN AGENDA:
 AddAgenda(Title of the agenda, Manager (who edits it), {Listeners (the ones who just see and follow the agenda)})
 ---------------------------------------------------------------------------*/
+AddAgenda("Gangster's Agenda", TEAM_MOB, {TEAM_GANG})
+AddAgenda("Police Agenda", TEAM_MAYOR, {TEAM_CHIEF, TEAM_POLICE})
 -- Example: AddAgenda("Gangster's agenda", TEAM_MOB, {TEAM_GANG})
 -- Example: AddAgenda("Police agenda", TEAM_MAYOR, {TEAM_CHIEF, TEAM_POLICE})
